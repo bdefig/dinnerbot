a4fdd213cbe06effebbb2368f755b7a4b6564765
Bryce DeFigueiredo <bdefig@gmail.com>
2016-09-25 23:22:30 -0600

    Yelp 29

--- index.js
+++ index.js
@@ -102,8 +102,9 @@ function sendRandomRestaurant(sender, city) {
 			console.log('Error receiving access token from Yelp: ', response.body.error)
 		} else {
 			// yelpToken = JSON.stringify(response.body).substring(0, 300)
-			yelpToken = response.body.access_token
+			// yelpToken = response.body.access_token
 			// yelpToken = JSON.stringify(response).substring(0, 300)
+			yelpToken = response.json.access_token
 			bearerText = 'Bearer ' + yelpToken
 			// sendTextMessage(sender, 'Yelp token received')
 			sendTextMessage(sender, JSON.stringify(response).substring(0, 300))
