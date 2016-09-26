622135522942038e60205c89ac02cda9c90fe1c7
Bryce DeFigueiredo <bdefig@gmail.com>
2016-09-26 01:28:27 -0600

    Yelp 49

--- index.js
+++ index.js
@@ -49,22 +49,6 @@ const token = process.env.FB_PAGE_ACCESS_TOKEN
 const yelpClientID = process.env.YELP_CLIENT_ID
 const yelpClientSecret = process.env.YELP_CLIENT_SECRET
 
-// function sendWelcomeMessage(sender) {
-//     messageData = {
-//         text : "Hello, first message"
-//     }
-//     request({
-// 		url:  "https://graph.facebook.com/v2.6/me/thread_settings",
-// 		qs: { access_token: token }, 
-// 		json: { "setting_type":"call_to_actions",
-// 	      "thread_state":"new_thread",
-// 	      "call_to_actions":[{"message":{"text":"Hello there!"}}]
-// 	    }, function(){
-// 	//call back
-// 		}
-// 	})
-// }
-
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
@@ -101,60 +85,27 @@ function sendRandomRestaurant(sender, city) {
 		} else if (response.body.error) {
 			console.log('Error receiving access token from Yelp: ', response.body.error)
 		} else {
-			// yelpToken = JSON.stringify(response.body).substring(0, 300)
-			// let thisBody = response.body
-			// yelpToken = thisBody.access_token
-			// yelpToken = JSON.stringify(response).substring(0, 300)
 			yelpToken = JSON.parse(body).access_token
 			bearerText = 'Bearer ' + yelpToken
-			// sendTextMessage(sender, 'Yelp token received')
-			// sendTextMessage(sender, JSON.stringify(response).substring(0, 300))
-			// sendTextMessage(sender, yelpToken)
-			// sendTextMessage(sender, bearerText)
-			// sendTextMessage(sender, yelpToken)
 		}
 	request({
 		url: 'https://api.yelp.com/v3/businesses/search',
-		// qs: {access_token:yelpToken},
-		// access_token: yelpToken,
 		headers: {'Authorization': bearerText},
-		// Authorization: bearerText,
 		method: 'GET',
 		qs: {location: city}
-		// json: {
-		// 	// access_token: yelpToken,
-		// 	location: city,
-		// }
 	}, function(error, response, body) {
 		if (error) {
 			console.log('Error sending to Yelp: ', error)
 		} else if (response.body.error) {
 			console.log('Error received from Yelp: ', response.body.error)
 		} else {
-			let businessName = JSON.parse(body).businesses[0].name
-			sendTextMessage(sender, businessName)
+			let businessArray = JSON.parse(body).businesses
+			let businessesLength = businessArray.length
+			low = 0
+			high = businessesLength
+			let businessNumber = Math.floor(Math.random() * (high - low + 1) + low)
+			sendTextMessage(sender, businessArray[businessNumber])
 		}
 	})
 	})
-	// request({
-	// 	url: 'https://api.yelp.com/v3/businesses/search',
-	// 	// qs: {access_token:yelpToken},
-	// 	// access_token: yelpToken,
-	// 	headers: {'Authorization': bearerText},
-	// 	// Authorization: bearerText,
-	// 	method: 'GET',
-	// 	json: {
-	// 		// access_token: yelpToken,
-	// 		'location': city,
-	// 	}
-	// }, function(error, response, body) {
-	// 	if (error) {
-	// 		console.log('Error sending to Yelp: ', error)
-	// 	} else if (response.body.error) {
-	// 		console.log('Error received from Yelp: ', response.body.error)
-	// 	} else {
-	// 		text = response.businesses[0].name
-	// 		sendTextMessage(sender, text)
-	// 	}
-	// })
 }
