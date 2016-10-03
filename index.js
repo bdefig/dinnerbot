077766d2696438d6ddd0c2d419a20923b546cf8a
Bryce DeFigueiredo <bdefig@gmail.com>
2016-10-03 11:40:14 -0600

    Get directions from Google -  use googlemaps module - fix install module

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const googlemaps = require('googlemaps')
+const GoogleMapsAPI = require('googlemaps')
 
 const token = process.env.FB_PAGE_ACCESS_TOKEN
 const yelpClientID = process.env.YELP_CLIENT_ID
